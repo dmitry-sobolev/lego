ce1e8fe1dfdfcfacfc2a71c1ea22346af9455511
Dmitry Sobolev <propp.ds@gmail.com>
2017-01-25 12:26:55 +0500

    Sleep 45 days, renew when 60 days left

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,9 +36,9 @@ fi
 
 if test "$cmd" = "renew"; then
     while test 1; do
-        sh -c "$app renew" && \
-        echo "[$(date)] Sleeping. Next renewal scheduled in 30 days" && \
-        sleep 30d
+        sh -c "$app renew --days 60" && \
+        echo "[$(date)] Sleeping. Next renewal scheduled in 45 days" && \
+        sleep 45d
     done
 fi
 
