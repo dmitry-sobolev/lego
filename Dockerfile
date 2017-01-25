b464c759c006607834490d5fb0ca00dd2f715379
Dmitry Sobolev <propp.ds@gmail.com>
2017-01-25 11:24:37 +0500

    Docker added

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM xenolf/lego
+
+ENV DO_AUTH_TOKEN='' \
+    ACME_EMAIL='' \
+    ACME_DOMAINS='' \
+    ACME_DNS_TYPE='digitalocean'
+
+WORKDIR /opt
+
+COPY ./entrypoint.sh /opt
+
+VOLUME ["/opt/.lego"]
+
+ENTRYPOINT ["/bin/sh", "entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+if test -z "$ACME_DNS_TYPE" -o -z "$ACME_EMAIL" -o -z "$ACME_DOMAINS"; then
+    echo "Env vars must be set"
+    exit 1
+fi
+
+cmd=$1
+
+if test -z "$cmd"; then
+    echo "Command must be set"
+    exit 1
+fi
+
+domains=$(echo "$ACME_DOMAINS" | tr "," "\n")
+domain_args=""
+
+for domain in $domains; do
+    if test -n $domain; then
+        domain_args="$domain_args -d $domain"
+    fi
+done
+
+if test ! -n $domain_args; then
+    echo "Empty domain list"
+    exit 1
+fi
+
+app="/usr/bin/lego -a -m $ACME_EMAIL $domain_args --dns $ACME_DNS_TYPE -k rsa4096 "
+
+if test "$cmd" = "run"; then
+    exec sh -c "$app run"
+fi
+
+if test "$cmd" = "renew"; then
+    while test 1; do
+        exec sh -c "$app renew"
+        echo "[$(date)] Sleeping. Next renewal scheduled in 30 days"
+        sleep 30d
+    done
+fi
+
+echo "Command must be either run or renew"
+exit 1
